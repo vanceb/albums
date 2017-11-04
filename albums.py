6558cf88b95ccc5364d47c6332629e29ae7998bc
Vance <vance@axxe.co.uk>
2017-11-04 16:12:39 +0000

    Basic working version

---
+++ albums.py
@@ -0,0 +1,328 @@
+import os
+import logging
+import argparse
+import plistlib             # To read iTunes export xml file    
+from tinytag import TinyTag # ID3 Tag reader
+
+
+def parse_commandline():
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.DEBUG)
+    parser = argparse.ArgumentParser(description='Compare albums from itunes against a file system')
+    parser.add_argument('-l',
+                        '--loglevel',
+                        dest='loglevel',
+                        action='store',
+                        default='INFO',
+                        choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'],
+                        required=False,
+                        help='The library xml file produced by iTunes->File->Library->Export Library'
+                        )
+    parser.add_argument('-x',
+                        '--xml',
+                        dest='xml_filename',
+                        action='store',
+                        default='library.xml',
+                        required=True,
+                        help='The library xml file produced by iTunes->File->Library->Export Library'
+                        )
+    parser.add_argument('-d',
+                        '--directory',
+                        dest='base_directory',
+                        action='store',
+                        default='.',
+                        help='The base directory containing your albums.  Defaults to current directory'
+                        )
+    args = parser.parse_args()
+    if args.loglevel.upper() == 'CRITICAL':
+        log.setLevel(logging.CRITICAL)
+    elif args.loglevel.upper() == 'ERROR  ':
+        log.setLevel(logging.ERROR)
+    elif args.loglevel.upper() == 'WARNING':
+        log.setLevel(logging.WARNING)
+    elif args.loglevel.upper() == 'INFO':
+        log.setLevel(logging.INFO)
+    elif args.loglevel.upper() == 'DEBUG':
+        log.setLevel(logging.DEBUG)
+    else:
+        log.error('Unexpected log level!')
+
+    log.debug('Commandline arguments: ' + str(args))
+    return args
+
+
+def artist_album_from_xml(filename):
+    log = logging.getLogger(__name__)
+    path = os.path.abspath(filename)
+    log.info('Opening ' + path)
+    data = plistlib.readPlist(path)
+
+    tracks = data['Tracks']
+    music = {}
+    artist = ''
+    album = ''
+    track_name = ''
+    for track_id in tracks:
+        track = tracks[track_id]
+        if 'Album Artist' in track:
+            artist = track['Album Artist']
+        elif 'Artist' in track:
+            artist = track['Artist']
+        else:
+            artist=None
+            log.warning("Unable to find Artist for track_id: " + track_id)
+
+        if artist is not None:
+            if 'Album' in track:
+                album = track['Album']
+            else:
+                album = ''
+                log.warning('Unable to get album for track_id: ' + track_id)
+
+            if 'Name' in track:
+                track_name = track['Name']
+            else:
+                track_name = ''
+                log.warning('Unable to get track name for track_id: ' + track_id)
+
+        if artist is not None:
+            if artist not in music:
+                music[artist] = {}
+            if album not in music[artist]:
+                music[artist][album] = [track_name]
+            else:
+                music[artist][album].append(track_name)
+            log.debug('Processed: ' + artist + '/' + album + '/' + track_name)
+
+    return music
+
+
+def artist_album_from_dirs(basedir):
+    log = logging.getLogger(__name__)
+    music_file_exts = ['.mp3', '.flac', '.ogg', '.wav', '.wma', '.mp4', '.m4a']
+    music = {}
+    artist = None
+    album = ''
+    track_name = ''
+    for dirName, subdirList, fileList in os.walk(basedir):
+        for fname in fileList:
+            path = os.path.abspath(dirName + '/' + fname)
+            name, ext = os.path.splitext(path)
+            if ext in music_file_exts:
+                tag = TinyTag.get(path)
+
+                if tag.albumartist is None or tag.albumartist == '':
+                    if tag.artist is None or tag.artist == '':
+                        log.warning('Unable to find artist for file: ' + path)
+                        artist = None
+                    else:
+                        artist = tag.artist
+                else:
+                    artist = tag.albumartist
+
+                if tag.album is None or tag.album == '':
+                    log.warning('Unable to get album for file: ' + path)
+                    album = ''
+                else:
+                    album = tag.album
+
+                if tag.title is None or tag.title == '':
+                    log.warning('Unable to get track name for file: ' + path)
+                    track_name = ''
+                else:
+                    track_name = tag.title
+
+
+                if artist is not None:
+                    if artist not in music:
+                        music[artist] = {}
+                    if album not in music[artist]:
+                        music[artist][album] = [track_name]
+                    else:
+                        music[artist][album].append(track_name)
+                    log.debug('Processed: ' + artist + '/' + album + '/' + track_name)
+
+    return music
+
+
+def tree_print(music):
+    artists = 0
+    albums = 0
+    tracks = 0
+    for artist in music:
+        print(artist)
+        artists += 1
+        for album in music[artist]:
+            print('\t' + album)
+            albums += 1
+            for track in music[artist][album]:
+                print('\t\t' + track)
+                tracks += 1
+
+    print()
+    print('Artists: ' + str(artists))
+    print('Albums: ' + str(albums))
+    print('Tracks: ' + str(tracks))
+    
+
+def aa_print(album_artist):
+    for aa in album_artist:
+        print(aa['artist'] + ' / ' + aa['album'])
+
+
+def normalise(txt):
+    return txt.strip()
+
+
+def check(test, reference):
+    log = logging.getLogger(__name__)
+    log.debug('Started to check test against reference')
+    # Create an index of the reference
+    ref_artist = {}     # Hash lookup of normalised artists to full artist names
+    ref_album = {}      # Hash lookup of normalised albums to full titles
+    album_artist = {}   # Lookup from album to list of artists
+    log.debug('Building reference indices')
+    log.debug('Creating artist index')
+    for artist in reference:
+        # Normalise the artist name and fill ref_artist
+        norm = normalise(artist)
+        log.debug('Normalised ' + artist + ' to ' + norm)
+        if norm in ref_artist:
+            if ref_artist[norm] != artist:  
+                log.debug('Additional entry for ' + norm + ' created for ' + artist)
+                ref_artist[norm].append(artist)
+            else:
+                log.debug('Artist ' + artist + ' already indexed, skipping')
+        else:
+            log.debug('New entry for ' + norm + ' created for ' + artist)
+            ref_artist[norm] = [artist]
+
+        # for each of the artists albums
+        log.debug('Creating album index for ' + artist)
+        for album in reference[artist]:
+            # Normalise the album name and fill ref_album
+            norm = normalise(album)
+            log.debug('Normalised ' + album + ' to ' + norm)
+            if norm in ref_album:
+                if ref_album[norm] != album:
+                    log.debug('Additional entry for ' + norm + ' created for ' + album) 
+                    ref_album[norm].append(album)
+                else:
+                    log.debug('Duplicate album name ' + album + ', skipping')
+            else:
+                log.debug('New entry for ' + norm + ' created for ' + album)
+                ref_album[norm] = [album]
+
+            # Create a reverse lookup from album to list of artists
+            log.debug('Creating reverse lookup of album to artist')
+            if album in album_artist:
+                log.debug('Additional entry for album ' + album + ' created for ' + artist)
+                album_artist[album].append(artist)
+            else:
+                log.debug('New entry for ' + norm + ' created for ' + artist)
+                album_artist[album] = [artist]
+
+    # Create some structures to keep track of matches or possibles
+    matched = []
+    hit_artist = []
+    hit_album = []
+    miss = []
+
+    # Loop over the test trying to look up against the reference
+    log.debug('Begining matching')
+    for artist in test:
+        log.debug('Artist: ' + artist)
+        # Look for the artist
+        norm = normalise(artist)
+        if norm in ref_artist:
+            log.debug('Found match for ' + artist)
+            found_artist = ref_artist[norm]
+        else:
+            log.debug('No match found for ' + artist)
+            found_artist = None
+
+        # Look for the album
+        log.debug('Album: ' + album)
+        for album in test[artist]:
+            norm = normalise(album)
+            if norm in ref_album:
+                log.debug('Found match for ' + album)
+                found_album = ref_album[norm]
+            else:
+                log.debug('No match found for ' + album)
+                found_album = None
+
+            # Make sense of the matches
+            if found_artist is not None:
+                # We matched a normalised artist
+                if found_album is not None:
+                    # We matched a normalised album
+                    # Need to check that the album and artist correlate
+                    # We may get back a list of artists or albums...
+                    log.debug('Matched artist and album')
+                    hit = 0
+                    poss = []
+                    for artist in found_artist:
+                        for album in found_album:
+                            try:
+                                # This will throw an exception if no correlation
+                                tracks = reference[artist][album]
+                                # No exception so we have a hit
+                                hit += 1
+                                poss.append({'artist': artist, 'album': album})
+                            except:
+                                # Do nothing, but continue to loop over
+                                # possibilities
+                                pass
+                    if hit == 0:
+                        # No correlation between the matched artist and album!
+                        log.info('No correlation: ' + artist + '/' + album)
+                        miss.append({'artist': artist, 'album': album})
+                    elif hit == 1:
+                        # A single correlation - assume correct
+                        log.info('Found a match: ' + artist + '/' + album)
+                        matched.append({'artist': poss[0]['artist'],
+                                       'album': poss[0]['album']})
+                    else:
+                        # Multiple possibilities
+                        log.info('Multiple hits: ' + artist + '/' + album)
+                        hit_album.append({'artist': artist, 'album': album})
+                else:
+                    # Hit the artist only
+                    log.info('Artist only: ' + artist + '/' + album)
+                    hit_artist.append({'artist': artist, 'album': album})
+            else:
+                # We didn't match a normalised artist
+                if found_album is not None:
+                    # We hit an album title
+                    log.info('Album only: ' + artist + '/' + album)
+                    hit_album.append({'artist': artist, 'album': album})
+                else:
+                    # Didn't hit anything
+                    log.info('Miss: ' + artist + '/' + album)
+                    miss.append({'artist': artist, 'album': album})
+
+    return matched, miss, hit_artist, hit_album
+
+
+def main():
+    logging.basicConfig()
+    log = logging.getLogger(__name__)
+    args = parse_commandline()
+    log.info('Started...')
+    log.info('Reading data from ' + args.xml_filename)
+    itunes = artist_album_from_xml(args.xml_filename)
+    log.info('Scanning ' + args.base_directory + ' for artists and albums')
+    disk = artist_album_from_dirs(args.base_directory)
+    
+    matched, miss, hit_artist, hit_album = check(itunes, disk)
+
+    aa_print(matched)
+
+    #tree_print(itunes)
+    #tree_print(disk)
+
+
+if __name__ == "__main__":
+    main()
+
